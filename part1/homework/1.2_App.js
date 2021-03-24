e900ecf234970d1cfe492b8371558ab80fea0997
abudri <17362519+abudri@users.noreply.github.com>
2021-03-24 16:54:51 -0400

    Update 1.2_App.js

--- part1/homework/1.2_App.js
+++ part1/homework/1.2_App.js
@@ -1,8 +1,6 @@
 // Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. Instead it only renders
 // three Part components of which each renders the name and number of exercises of one part.
 
-
-
 import React from 'react'
 
 const Header = (props) => {
